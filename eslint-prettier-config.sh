3e6282c6bd1a838d3074bf376e03e011e67000b9
Ulric Esparra <Ulric.Esparra@gmail.com>
2021-06-16 12:21:08 -0600

    Update eslint-prettier-config.sh

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -82,140 +82,36 @@ echo
 echo -e "${GREEN}Configuring your development environment... ${NC}"
 
 echo
-echo -e "1/4 ${LCYAN}ESLint & Prettier Installation... ${NC}"
+echo -e "1/3 ${LCYAN}ESLint & Prettier Installation... ${NC}"
 echo
-$pkg_cmd -D eslint prettier eslint-plugin-react-hooks
-
-echo
-echo -e "2/4 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
-echo
-$pkg_cmd -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
-
-echo
-echo -e "3/4 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
-echo "See https://github.com/prettier/eslint-config-prettier for more details."
-echo
-$pkg_cmd -D eslint-config-prettier eslint-plugin-prettier
+$pkg_cmd -D eslint prettier eslint-config-airbnb-typescript-prettier
 
 
 if [ "$skip_eslint_setup" == "true" ]; then
   break
 else
   echo
-  echo -e "4/4 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
+  echo -e "2/3 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
   > ".eslintrc${config_extension}" # truncates existing file (or creates empty)
 
   echo ${config_opening}'
   "extends": [
-    "airbnb",
-    "prettier",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react-hooks/recommended"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "impliedStrict": true,
-      "classes": true
-    }
-  },
-  "env": {
-    "browser": true,
-    "node": true,
-    "jquery": true,
-    "jest": true
-  },
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-unused-vars": 1,
-    "prefer-const": [
-      "error",
-      {
-        "destructuring": "all"
-      }
-    ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
-    "no-unused-expressions": [
-      2,
-      {
-        "allowTaggedTemplates": true
-      }
-    ],
-    "no-param-reassign": [
-      2,
-      {
-        "props": false
-      }
-    ],
-    "no-console": 0,
-    "import/prefer-default-export": 1,
-    "import": 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "react/display-name": 1,
-    "react/no-array-index-key": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/no-unescaped-entities": 0,
-    "jsx-a11y/accessible-emoji": 0,
-    "react/require-default-props": 0,
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    ],
-    "radix": 0,
-    "no-shadow": "off",
-    "quotes": [
-      2,
-      "single",
-      {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        "trailingComma": "'${trailing_comma_pref}'",
-        "singleQuote": true,
-        "printWidth": '${max_len_val}'
-      }
-    ],
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
-      {
-        "aspects": [
-          "invalidHref"
-        ]
-      }
-    ]
-  },
-  "plugins": [
-    "prettier",
-    "react",
-    "react-hooks"
+    "airbnb-typescript-prettier"
   ]
 }' >> .eslintrc${config_extension}
+fi
+
+if [ "$skip_prettier_setup" == "true" ]; then
+  break
+else
+  echo -e "3/3 ${YELLOW}Building your .prettierrc${config_extension} file... ${NC}"
+  > .prettierrc${config_extension} # truncates existing file (or creates empty)
+
+  echo ${config_opening}'
+  "printWidth": '${max_len_val}',
+  "singleQuote": true,
+  "trailingComma": "'${trailing_comma_pref}'"
+}' >> .prettierrc${config_extension}
 fi
 
 echo
