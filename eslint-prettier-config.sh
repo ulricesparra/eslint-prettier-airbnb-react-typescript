26b23dd6414e4fd0a1f63be72b8e79f0139be425
Ulric Esparra <Ulric.Esparra@gmail.com>
2021-06-28 14:18:37 -0600

    Update eslint-prettier-config.sh

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -12,19 +12,6 @@ NC='\033[0m' # No Color
 # --------------------------------------
 # Prompts for configuration preferences
 # --------------------------------------
-
-# Package Manager Prompt
-echo
-echo "Which package manager are you using?"
-select package_command_choices in "Yarn" "npm" "Cancel"; do
-  case $package_command_choices in
-    Yarn ) pkg_cmd='yarn add'; break;;
-    npm ) pkg_cmd='npm install'; break;;
-    Cancel ) exit;;
-  esac
-done
-echo
-
 # Checks for existing eslintrc files
 if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; then
   echo -e "${RED}Existing ESLint config file(s) found:${NC}"
@@ -32,37 +19,12 @@ if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".esli
   echo
   echo -e "${RED}CAUTION:${NC} there is loading priority when more than one config file is present: https://eslint.org/docs/user-guide/configuring#configuration-file-formats"
   echo
-  read -p  "Write .eslintrc${config_extension} (Y/n)? "
+  read -p  "Write .eslintrc (Y/n)? "
   if [[ $REPLY =~ ^[Nn]$ ]]; then
     echo -e "${YELLOW}>>>>> Skipping ESLint config${NC}"
     skip_eslint_setup="true"
   fi
 fi
-finished=false
-
-# Max Line Length Prompt
-while ! $finished; do
-  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 100) "
-  if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
-    max_len_val=$REPLY
-    finished=true
-    echo
-  else
-    echo -e "${YELLOW}Please choose a max length of two or three digits, e.g. 80 or 100 or 120${NC}"
-  fi
-done
-
-# Trailing Commas Prompt
-echo "What style of trailing commas do you want to enforce with Prettier? (Recommendation: es5)"
-echo -e "${YELLOW}>>>>> See https://prettier.io/docs/en/options.html#trailing-commas for more details.${NC}"
-select trailing_comma_pref in "none" "es5" "all"; do
-  case $trailing_comma_pref in
-    "none" ) break;;
-    "es5" ) break;;
-    "all" ) break;;
-  esac
-done
-echo
 
 # ----------------------
 # Perform Configuration
@@ -73,7 +35,7 @@ echo -e "${GREEN}Configuring your development environment... ${NC}"
 echo
 echo -e "1/3 ${LCYAN}ESLint & Prettier Installation... ${NC}"
 echo
-$pkg_cmd -D eslint prettier eslint-config-airbnb-typescript-prettier
+npm i -D eslint prettier eslint-config-airbnb-typescript-prettier
 
 
 if [ "$skip_eslint_setup" == "true" ]; then
@@ -97,9 +59,9 @@ else
   > .prettierrc # truncates existing file (or creates empty)
 
   echo '{
-  "printWidth": '${max_len_val}',
+  "printWidth": '100',
   "singleQuote": true,
-  "trailingComma": "'${trailing_comma_pref}'"
+  "trailingComma": "'es5'"
 }' >> .prettierrc
 fi
 
